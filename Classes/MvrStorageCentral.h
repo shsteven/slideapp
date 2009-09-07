fcc3b19a631a10dc24b17cea3ca01eaa3542666a
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-09-07 17:38:25 +0200

    First appaerance of the Storage Central. Does not compile cleanly (needed changes to L0MoverItem are missing).

---
+++ Classes/MvrStorageCentral.h
@@ -0,0 +1,103 @@
+//
+//  MvrStorageCentral.h
+//  Mover
+//
+//  Created by ∞ on 07/09/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "L0MoverItem.h"
+
+@interface MvrStorageCentral : NSObject {
+	NSMutableSet* storedItems;
+	NSMutableDictionary* metadata;
+}
+
++ sharedCentral;
+
+// Add an item here to save it to persistent storage. Remove it from here to remove it from persistent storage. The item storage object used by this item will be made persistent on addition and nonpersistent on removal.
+// The item may be asked to immediately clear its cache. You should access the item (eg to produce thumbnails) BEFORE putting it into storage, to avoid the item's content being needlessly reloaded from disk.
+- (void) addStoredItemsObject:(L0MoverItem*) item;
+- (void) removeStoredItemsObject:(L0MoverItem*) item;
+@property(readonly) NSSet* storedItems;
+
+@end
+
+
+// -----
+
+enum {
+	kMvrStorageDestinationMemory,
+	kMvrStorageDestinationDisk,
+};
+typedef NSUInteger MvrStorageDestination;
+
+@interface MvrItemStorage : NSObject {
+	BOOL persistent;
+	unsigned long long contentLength;
+	
+	NSData* data;
+	NSString* path;
+	
+	NSOutputStream* lastOutputStream;
+}
+
+// Creating a new item storage.
++ itemStorage; // a new empty one.
++ itemStorageWithData:(NSData*) data;
++ itemStorageFromFileAtPath:(NSString*) path error:(NSError**) e; // If not in NSTemporaryDirectory(), it might be copied.
+// + itemStorageWithContentsOfStream:(NSInputStream*) stream;
+
+// If NO, the contents will be lost when the item storage is deallocated.
+// You cannot set this property. Instead, add an item with this storage to the storage central.
+@property(readonly, getter=isPersistent) BOOL persistent;
+
+// The size in bytes of the content of this storage. (See 'Writing' for caveats.)
+@property unsigned long long contentLength;
+
+// Causes the item storage to clear its cache and remove all in-memory content by offloading it to disk.
+- (void) clearCache;
+
+#pragma mark -
+#pragma mark Reading from item storage
+// These methods are independent of the actual kind of storage (in memory or on disk) used for this storage.
+// The above means that if you call -data and the item was saved on a file on disk, it will be loaded. This is usually not the desired behavior if the item is big. See below for opportunistic reading methods.
+
+@property(copy) NSData* data;
+
+// The whole content of the storage as a NSData object. (For info on setting, see below in the 'Writing' section.)
+- (NSData*) data;
+
+// The path to an app-accessible file on disk that contains the item's contents. Exists for as long as this object does if no undue interference happens.
+- (NSString*) path;
+
+// YES if this object has written data to a path on disk, NO if it only exists in memory (and would be offloaded by a call to -path).
+@property(readonly) BOOL hasPath;
+
+// An input stream reading from the content of the storage. Returned unopened.
+- (NSInputStream*) inputStream;
+
+// Opportunistic reading. These methods are used to choose a "good", performance-wise and memory-wise, way to access the storage's content.
+
+// Returns the 'best' way you can access the storage's contents. This is either a NSData or a NSInputStream object. Roughly speaking, if the object is already entirely in memory, it will exploit this by returning the NSData object; but if not, it will instead return a stream to that data on disk (or wherever it is stored).
+- (id) preferredContentObject;
+
+// Returns a NSData object if the contentLength is equal or less to the limit, otherwise nil.
+- (NSData*) dataIfLengthIsAtMost:(unsigned long long) maximumLength;
+
+#pragma mark -
+#pragma mark Writing to item storage
+// All writing methods clear whatever contents the item storage previously held when called.
+
+// Returns an unopened output stream. Write to this stream to set the contents of the item storage. The size for ...OfAssumedSize: will be used as a hint to the item storage to choose where to store what is written to the stream. Otherwise, if the size is unknown but the order of magnitude is, you can use ...ForStorageIn: to choose where to save the stream yourself.
+// When you have finished writing to the stream and closed it, you MUST call the -endUsingOutputStream method to let the item storage take ownership of the data you wrote.
+// It's a programmer error to use ANY method or access ANY property of this object before using -endUsingOutputStream OR to call it before closing the stream. Behavior is undefined in either case.
+- (NSOutputStream*) outputStreamForContentOfAssumedSize:(unsigned long long) size;
+- (NSOutputStream*) outputStreamForStorageIn:(MvrStorageDestination) destination;
+- (void) endUsingOutputStream;
+
+// Setting the .data property causes the whole content of the storage item to be reset to whatever you passed. Depending on the length of the data, it might immediately be written to disk rather than keeping a copy in memory (with similar size limits than those used for outputStreamForContentOfAssumedSize:).
+- (void) setData:(NSData*) data;
+
+@end
---
+++ Classes/MvrStorageCentral.m
@@ -0,0 +1,382 @@
+//
+//  MvrStorageCentral.m
+//  Mover
+//
+//  Created by ∞ on 07/09/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "MvrStorageCentral.h"
+#import <MuiKit/MuiKit.h>
+
+#import "L0MoverAppDelegate.h"
+
+// 1 MB or more of data? go straight to disk whenever possible.
+#define kMvrItemStorageMaximumAmountOfDataBeforeOffloading 1024 * 1024
+
+@interface MvrItemStorage ()
++ itemStorageFromFileAtPath:(NSString*) path persistent:(BOOL) persistent error:(NSError**) e;
+- (void) resetData;
+@property(copy, setter=private_setPath:) NSString* path;
+@property(assign, setter=private_setPersistent:) BOOL persistent;
+
+@end
+
+@interface MvrStorageCentral ()
+
++ (NSString*) unusedTemporaryFileName;
++ (NSString*) unusedPathInDirectory:(NSString*) path fileName:(NSString**) name;
+- (void) saveMetadata;
+
+@end
+
+static BOOL MvrFileIsInDirectory(NSString* file, NSString* directory) {
+	NSArray* dirParts = [[directory stringByStandardizingPath] pathComponents],
+		* fileParts = [[file stringByStandardizingPath] pathComponents];
+	
+	if ([dirParts count] <= [fileParts count])
+		return NO;
+	
+	int i = 0; for (NSString* part in dirParts) {
+		if (![[fileParts objectAtIndex:i] isEqual:part])
+			return NO;
+		i++;
+	}
+	
+	return YES;
+}
+
+#pragma mark -
+#pragma mark Storage Central.
+
+
+@implementation MvrStorageCentral
+
+L0ObjCSingletonMethod(sharedCentral)
+
+- (id) init;
+{
+	if (self = [super init])
+		metadata = [NSMutableDictionary new];
+	
+	return self;
+}
+
+- (void) dealloc;
+{
+	[metadata release];
+	[storedItems release];
+	[super dealloc];
+}
+
++ (NSString*) unusedTemporaryFileName;
+{
+	return [self unusedPathInDirectory:NSTemporaryDirectory() fileName:NULL];
+}
+
++ (NSString*) unusedPathInDirectory:(NSString*) path fileName:(NSString**) name;
+{
+	NSFileManager* fm = [NSFileManager defaultManager];
+	NSString* newPath, * uuidName;
+	do {
+		uuidName = [[L0UUID UUID] stringValue];
+		newPath = [path stringByAppendingPathComponent:uuidName];
+	} while ([fm fileExistsAtPath:newPath]);
+	
+	if (name) *name = uuidName;
+	return newPath;
+}
+
+- (NSSet*) storedItems;
+{
+	if (storedItems)
+		return storedItems;
+	
+	storedItems = [NSMutableSet new];
+	[metadata removeAllObjects];
+
+	NSDictionary* storedMetadata = [[NSUserDefaults standardUserDefaults] dictionaryForKey:@"L0SlidePersistedItems"];
+	if (!storedMetadata)
+		return storedItems; // empty
+	
+	NSString* docs = [L0Mover documentsDirectory];
+	
+	for (NSString* name in storedMetadata) {
+		NSDictionary* itemInfo = [storedMetadata objectForKey:name];
+		if (![itemInfo isKindOfClass:[NSDictionary class]])
+			continue;
+		
+		NSString* title = [itemInfo objectForKey:@"Title"];
+		NSString* type = [itemInfo objectForKey:@"Type"];
+		
+		if (!title || !type)
+			continue;
+		
+		NSString* path = [docs stringByAppendingPathComponent:name];
+		
+		NSError* e;
+		MvrItemStorage* itemStorage = [MvrItemStorage itemStorageFromFileAtPath:path persistent:YES error:&e];
+		if (!itemStorage) {
+			L0LogAlways(@"%@", e);
+		} else {
+			L0MoverItem* item = [L0MoverItem itemWithStorage:itemStorage type:type title:title];
+			if (item) {
+				[storedItems addObject:item];
+				[metadata setObject:[NSDictionary dictionaryWithObjectsAndKeys:
+									 title, @"Title",
+									 type, @"Type",
+									 nil] forKey:name];
+			}
+		}
+	}
+	
+	return storedItems;
+}
+
+- (void) addStoredItemsObject:(L0MoverItem *)item;
+{
+	MvrItemStorage* storage = [item storage];
+	NSString* path, * name;
+	path = [[self class] unusedPathInDirectory:[L0Mover documentsDirectory] fileName:&name];
+	
+	NSError* e;
+	BOOL done = [[NSFileManager defaultManager] moveItemAtPath:storage.path toPath:path error:&e];
+	if (!done) {
+		L0LogAlways(@"%@", e);
+		return;
+	}
+	
+	[metadata setObject:[NSDictionary dictionaryWithObjectsAndKeys:
+						 item.title, @"Title",
+						 item.type, @"Type",
+						 nil] forKey:name];
+	[self saveMetadata];
+	
+	storage.path = path;
+	storage.persistent = YES;
+	
+	[storedItems addObject:item];
+}
+
+- (void) removeStoredItemsObject:(L0MoverItem*) item;
+{
+	if (![storedItems containsObject:item])
+		return;
+	
+	[storedItems removeObject:item];
+
+	MvrItemStorage* storage = [item storage];
+	if (storage.hasPath) {
+		NSString* path = storage.path, * name = [path lastPathComponent],
+			* newPath = [[self class] unusedTemporaryFileName];
+		
+		[metadata removeObjectForKey:name];
+		[self saveMetadata];
+		
+		NSError* e;
+		BOOL done = [[NSFileManager defaultManager] moveItemAtPath:path toPath:newPath error:&e];
+		if (!done)
+			L0LogAlways(@"%@", e);
+		
+		storage.path = newPath;
+	}
+	
+	storage.persistent = NO;
+}
+
+- (void) saveMetadata;
+{
+	NSUserDefaults* ud = [NSUserDefaults standardUserDefaults];
+	[ud setObject:metadata forKey:@"L0SlidePersistedItems"];
+	[ud synchronize];
+}
+
+@end
+
+#pragma mark -
+#pragma mark Item storage.
+
+
+@implementation MvrItemStorage
+
++ itemStorage;
+{
+	return [[self new] autorelease];
+}
+
++ itemStorageWithData:(NSData*) data;
+{
+	MvrItemStorage* me = [self itemStorage];
+	me.data = data;
+	return me;
+}
+
++ itemStorageFromFileAtPath:(NSString*) path error:(NSError**) e;
+{
+	return [self itemStorageFromFileAtPath:path persistent:NO error:e];
+}
+
+- (void) dealloc;
+{
+	if (!persistent && path) {
+		L0Log(@"Deleting offloading file %@", path);
+		[[NSFileManager defaultManager] removeItemAtPath:path error:NULL];
+	}
+	
+	[data release];
+	[path release];
+	[lastOutputStream release];
+	[super dealloc];
+}
+
++ itemStorageFromFileAtPath:(NSString*) path persistent:(BOOL) persistent error:(NSError**) e;
+{
+	NSFileManager* fm = [NSFileManager defaultManager];
+	if (![fm attributesOfItemAtPath:path error:e])
+		return nil;
+	
+	// if persistent == YES, we simply assume the file is already in persistent storage.
+	// Otherwise...
+	if (!persistent && !MvrFileIsInDirectory(path, NSTemporaryDirectory())) {
+		NSString* newPath = [MvrStorageCentral unusedTemporaryFileName];
+		
+		if (![fm copyItemAtPath:path toPath:newPath error:e])
+			return nil;
+		
+		path = newPath;
+	}
+	
+	MvrItemStorage* me = [self itemStorage];
+	me.path = path;
+	return me;
+}
+
+@synthesize persistent;
+
+- (unsigned long long) contentLength;
+{
+	if (data)
+		return [data length];
+	
+	return contentLength;
+}
+
+- (NSData*) data;
+{
+	if (data)
+		return data;
+	
+	return [NSData dataWithContentsOfMappedFile:path];
+}
+
+- (void) setData:(NSData*) d;
+{
+	if (d != data) {
+		[self resetData];
+		
+		[data release]; data = nil;
+		data = [d copy];
+		
+		if ([data length] > kMvrItemStorageMaximumAmountOfDataBeforeOffloading)
+			[self clearCache];
+	}
+}
+
+- (NSString*) path;
+{
+	if (!path)
+		[self clearCache];
+	
+	return path;
+}
+
+- (NSInputStream*) inputStream;
+{
+	if (data)
+		return [NSInputStream inputStreamWithData:data];
+	else
+		return [NSInputStream inputStreamWithFileAtPath:path];
+}
+
+- (id) preferredContentObject;
+{
+	if (path && !data)
+		return [self inputStream];
+	else
+		return data;
+}
+
+- (NSData*) dataIfLengthIsAtMost:(unsigned long long) maximumLength;
+{
+	if (self.contentLength <= maximumLength)
+		return self.data;
+	else
+		return nil;
+}
+
+- (NSOutputStream*) outputStreamForContentOfAssumedSize:(unsigned long long) size;
+{
+	MvrStorageDestination d = (size > kMvrItemStorageMaximumAmountOfDataBeforeOffloading)?
+		kMvrStorageDestinationDisk : kMvrStorageDestinationMemory;
+	return [self outputStreamForStorageIn:d];
+}
+
+- (NSOutputStream*) outputStreamForStorageIn:(MvrStorageDestination) destination;
+{
+	if (destination == kMvrStorageDestinationMemory) {
+		lastOutputStream = [[NSOutputStream outputStreamToMemory] retain];
+		return lastOutputStream;
+	} else {
+		self.path = [MvrStorageCentral unusedTemporaryFileName];
+		return [NSOutputStream outputStreamToFileAtPath:self.path append:NO];
+	}
+}
+
+- (void) endUsingOutputStream;
+{
+	if (lastOutputStream) {
+		self.data = [lastOutputStream propertyForKey:NSStreamDataWrittenToMemoryStreamKey];
+		[lastOutputStream release]; lastOutputStream = nil;
+	} else {
+		NSError* e;
+		id contentLengthObject = [[[NSFileManager defaultManager] attributesOfItemAtPath:path error:&e] objectForKey:NSFileSize];
+		if (!contentLengthObject)
+			[NSException raise:@"MvrStorageException" format:@"Could not find out the new size for the offloading file. Error: %@", e];
+		
+		contentLength = [contentLengthObject unsignedLongLongValue];
+	}
+}
+
+- (void) clearCache;
+{
+	if (!data) return;
+	
+	if (!path) // we'd have a path if we had been made persistant by the storage central.
+		self.path = [MvrStorageCentral unusedTemporaryFileName];
+	
+	NSError* e;
+	BOOL done = [data writeToFile:path options:NSAtomicWrite error:&e];
+	if (!done)
+		[NSException raise:@"MvrStorageException" format:@"Could not clear cache for this storage item: %@ (error: %@)", self ,e];
+}
+
+- (void) resetData;
+{
+	if (path) {
+		[[NSFileManager defaultManager] removeItemAtPath:path error:NULL];
+		self.path = nil;
+	}
+	
+	if (data) {
+		[data release]; data = [NSData new];
+	}
+}
+
+@synthesize contentLength, path;
+
+- (BOOL) hasPath;
+{
+	return path != nil;
+}
+
+@end
