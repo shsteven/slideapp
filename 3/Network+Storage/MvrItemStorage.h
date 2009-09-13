a596b2d1a8a69ce1b723286b90ece5ccc204fddb
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-09-13 09:57:01 +0200

    Changed subproject to "Network+Storage" since the two are so deeply entwined it makes no sense to really keep them separate.

---
+++ 3/Network+Storage/MvrItemStorage.h
@@ -0,0 +1,16 @@
+//
+//  MvrItemStorage.h
+//  Network
+//
+//  Created by ∞ on 12/09/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MvrItemStorage : NSObject {
+
+}
+
+@end
---
+++ 3/Network+Storage/MvrItemStorage.m
@@ -0,0 +1,14 @@
+//
+//  MvrItemStorage.m
+//  Network
+//
+//  Created by ∞ on 12/09/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "MvrItemStorage.h"
+
+
+@implementation MvrItemStorage
+
+@end
