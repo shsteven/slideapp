9529cb9b8d419d941bafe85af87011ee6ec42be0
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-09-14 11:03:29 +0200

    Many changes to mvr-wifi to add smarter logging of transfers.

--- 3/Network+Storage/mvr-wifi.m
+++ 3/Network+Storage/mvr-wifi.m
@@ -10,10 +10,23 @@
 #import <DDCommandLineInterface/DDCommandLineInterface.h>
 
 #import "MvrWiFi.h"
+#import "MvrModernWiFi.h"
 
 #import "MvrItemStorage.h"
 #import "MvrItem.h"
 #import "MvrUTISupport.h"
+
+#import <MuiKit/MuiKit.h>
+
+static const NSKeyValueObservingOptions kMvrKVOOptions = NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld|NSKeyValueObservingOptionInitial;
+
+static id MvrBlockForLoggingPropertyChange(NSString* propertyName) {
+	id block = ^(id object, NSDictionary* change) {
+		L0Log(@"%@.%@ == %@ (from %@)", object, propertyName, L0KVOChangedValue(change), L0KVOPreviousValue(change));
+	};
+	
+	return [[block copy] autorelease];
+}
 
 @interface MvrWiFiTool : NSObject <DDCliApplicationDelegate>
 {
@@ -21,6 +34,8 @@
 	NSString* temporary, * persistent, * identifier;
 	MvrWiFi* wifi;
 	L0KVODispatcher* kvo;
+	
+	int port;
 	
 	BOOL stopped;
 }
@@ -37,9 +52,16 @@
 - (id) makeItemWithFile:(NSString*) f type:(NSString*) type;
 - (id) makeItemWithFile:(NSString*) f;
 
+- (void) updateLoggingForChangesOfKeys:(NSSet*) keys inSetChanges:(NSDictionary*) kvoChanges;
+
 @end
 
 @implementation MvrWiFiTool
+
+- (void) setPort:(NSString *) portString;
+{
+	port = [portString intValue];
+}
 
 - (void) application: (DDCliApplication *) app
     willParseOptions: (DDGetoptLongParser *) optionParser;
@@ -48,8 +70,9 @@
     {
         // Long, Short, Argument options
         {@"name", 'n', DDGetoptRequiredArgument},
+        {@"port", 'p', DDGetoptRequiredArgument},
         {@"temporary", 't', DDGetoptRequiredArgument},
-        {@"persistent", 'p', DDGetoptRequiredArgument},
+        {@"persistent", 'P', DDGetoptRequiredArgument},
         {@"identifier", 'I', DDGetoptRequiredArgument},
 		{nil, 0, 0}
     };
@@ -66,8 +89,9 @@
 	
 	if (!self.identifier)
 		self.identifier = @"net.infinite-labs.Mover.TestTools.WiFi";
-	
+		
 	NSConnection* c = [[NSConnection serviceConnectionWithName:self.identifier rootObject:self] retain];
+	L0Log(@" == Wi-Fi tool registered on connection %@ with name '%@'", c, self.identifier);
 	
 	MvrStorageSetTemporaryDirectory(self.temporary);
 	MvrStorageSetPersistentDirectory(self.persistent);
@@ -76,18 +100,14 @@
 	
 	self.wifi = [[MvrWiFi alloc] initWithBroadcastedName:self.name];
 	
-	[kvo observe:@"enabled" ofObject:wifi.modernWiFi options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld usingBlock:^(id obj, NSDictionary* change) {
-		L0Log(@"%@", change);
-	}];
+	if (port != 0)
+		self.wifi.modernWiFi.serverPort = port;
 	
-	[kvo observe:@"channels" ofObject:wifi.modernWiFi usingSelector:@selector(channelsOfObject:changed:) options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld];
+	[kvo observe:@"enabled" ofObject:wifi.modernWiFi options:kMvrKVOOptions usingBlock:MvrBlockForLoggingPropertyChange(@"enabled")];
 	
-	[kvo observe:@"jammed" ofObject:wifi.modernWiFi options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld usingBlock:
-	 ^(id object, NSDictionary* change) {
-			
-		L0Log(@"%@", change);
-		
-	 }];
+	[kvo observe:@"channels" ofObject:wifi.modernWiFi usingSelector:@selector(channelsOfObject:changed:) options:kMvrKVOOptions];
+	
+	[kvo observe:@"jammed" ofObject:wifi.modernWiFi  options:kMvrKVOOptions usingBlock:MvrBlockForLoggingPropertyChange(@"jammed")];
 	
 	self.wifi.modernWiFi.enabled = YES;
 	
@@ -106,26 +126,49 @@
 }
 
 - (void) channelsOfObject:(id) modernWiFi changed:(NSDictionary*) change;
-{
+{	
 	L0Log(@"%@", change);
 	[kvo forEachSetChange:change
 	  invokeBlockForInsertion:
 	 ^(id added){
 	  
-		 [kvo observe:@"outgoingTransfers" ofObject:added usingSelector:@selector(outgoingTransfersOfObject:changed:) options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld];
+		 [kvo observe:@"outgoingTransfers" ofObject:added options:kMvrKVOOptions
+		   usingBlock:^(id outgoing, NSDictionary* change) {
+			   [self updateLoggingForChangesOfKeys:[NSSet setWithObjects:@"progress", @"finished", nil] inSetChanges:change]; 
+		   }];
+		 [kvo observe:@"incomingTransfers" ofObject:added options:kMvrKVOOptions
+		   usingBlock:^(id outgoing, NSDictionary* change) {
+			   [self updateLoggingForChangesOfKeys:[NSSet setWithObjects:@"progress", @"cancelled", @"item", nil] inSetChanges:change]; 
+		   }];
 		 
+		 [kvo observe:@"hasOutgoingTransfers" ofObject:added options:kMvrKVOOptions usingBlock:MvrBlockForLoggingPropertyChange(@"hasOutgoingTransfers")];
 	 }
 	  removal:
 	 ^(id removed) {
 		 
 		 [kvo endObserving:@"outgoingTransfers" ofObject:removed];
-		 		 
+		 [kvo endObserving:@"incomingTransfers" ofObject:removed];
+		 [kvo endObserving:@"hasOutgoingTransfers" ofObject:removed];
+		 
 	 }];
 }
 
-- (void) outgoingTransfersOfObject:(id) modernWiFi changed:(NSDictionary*) change;
+- (void) updateLoggingForChangesOfKeys:(NSSet*) keys inSetChanges:(NSDictionary*) kvoChanges;
 {
-	L0Log(@"%@", change);
+	[kvo forEachSetChange:kvoChanges
+	  invokeBlockForInsertion: ^(id added) {
+		  
+		  for (NSString* key in keys)
+			  [kvo observe:key ofObject:added options:kMvrKVOOptions usingBlock:MvrBlockForLoggingPropertyChange(key)];
+		  
+		  
+	  } 
+	  removal: ^(id removed) {
+		  
+		  for (NSString* key in keys)
+			  [kvo endObserving:key ofObject:removed];
+		  
+	  }];
 }
 
 - (id) makeItemWithFile:(NSString*) f type:(NSString*) type;
