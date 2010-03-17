199ec4d78e608802564d6d94d2e01c443db2eb98
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2010-03-17 14:36:56 +0100

    Added debug logging to generic items, to detect fallbacks.

--- 3/Network+Storage/MvrGenericItem.m
+++ 3/Network+Storage/MvrGenericItem.m
@@ -23,6 +23,12 @@
 	return [self initWithStorage:s type:(id) kUTTypeData metadata:m];
 }
 
+- (id) initWithStorage:(MvrItemStorage *)s type:(NSString *)t metadata:(NSDictionary *)m;
+{
+	L0Log(@"Will create a generic item for storage %@ of type %@, metadata %@", s, t, m);
+	return [super initWithStorage:s type:t metadata:m];
+}
+
 - (id) produceExternalRepresentation;
 {
 	return [self.storage preferredContentObject];
